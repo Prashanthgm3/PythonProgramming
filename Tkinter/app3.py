5fa4d138331b5f66b7f6164c324a7008ce524ae1
Prashanth Manohar G <prashanthmanohar.g@outlook.com>
2024-10-06 17:21:25 +0530

    worked on edit editor crud operations

--- Tkinter/app3.py
+++ Tkinter/app3.py
@@ -1,8 +1,13 @@
 import tkinter as tk
-
+from tkinter import ttk
 root= tk.Tk()
 
-tk.Label(root, text="Label 1", bg="green").pack(side="left", fill="both", expand=True)
-tk.Label(root, text="Label 1", bg="red").pack(side="top", fill="both")
+main = ttk.Frame(root)
+main.pack(side="left", fill="both", expand=True)
+
+tk.Label(main, text="Label top", bg="red").pack(side="top", fill="both", expand=True)
+tk.Label(main, text="Label top", bg="red").pack(side="top", fill="both", expand=True)
+
+tk.Label(root, text="Label left", bg="green").pack(side="left", fill="both", expand=True)
 
 root.mainloop()
